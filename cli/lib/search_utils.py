d64539f6f4200d37267a1acb14d5d91488f57159
skullzado <pafcalzado@gmail.com>
2026-01-21 07:25:20 +0800

    Refactor code, add InvertedIndex class

--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -6,6 +6,20 @@ DEFAULT_SEARCH_LIMIT = 5
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
 STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
+CACHE_DIR = os.path.join(PROJECT_ROOT, "cache")
+INDEX_PATH = os.path.join(PROJECT_ROOT, "cache", "index.pkl")
+DOCMAP_PATH = os.path.join(PROJECT_ROOT, "cache", "docmap.pkl")
+
+
+def build_cache_dir() -> None:
+    try:
+        os.makedirs(CACHE_DIR)
+    except FileExistsError:
+        print(f"Directory {CACHE_DIR} already exists.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{CACHE_DIR}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 def load_movies() -> list[dict]:
