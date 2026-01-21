24b539e20d954144b43fc88f53824a46facd6a14
skullzado <pafcalzado@gmail.com>
2026-01-21 13:13:54 +0800

    Add tf_idf func, tfidf command

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -7,6 +7,7 @@ from lib.keyword_search import (
     idf_command,
     search_command,
     tf_command,
+    tfidf_command,
 )
 
 
@@ -30,6 +31,12 @@ def main() -> None:
     )
     idf_parser.add_argument("term", type=str, help="Term to get IDF for")
 
+    tf_idf_parser = subparsers.add_parser(
+        "tfidf", help="Get TF-IDF score for a given document ID and term"
+    )
+    tf_idf_parser.add_argument("doc_id", type=int, help="Document ID")
+    tf_idf_parser.add_argument("term", type=str, help="Term to get TF-IDF score for")
+
     args = parser.parse_args()
 
     match args.command:
@@ -48,6 +55,11 @@ def main() -> None:
         case "idf":
             idf = idf_command(args.term)
             print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
+        case "tfidf":
+            tf_idf = tfidf_command(args.doc_id, args.term)
+            print(
+                f"TF-IDF score of '{args.term}' in document '{args.doc_id}': {tf_idf:.2f}"
+            )
         case _:
             parser.print_help()
 
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -74,6 +74,11 @@ class InvertedIndex:
         term_doc_count = len(self.index[token])
         return math.log((doc_count + 1) / (term_doc_count + 1))
 
+    def get_tf_idf(self, doc_id: int, term: str) -> float:
+        tf = self.get_tf(doc_id, term)
+        idf = self.get_idf(term)
+        return tf * idf
+
 
 def build_command() -> None:
     idx = InvertedIndex()
@@ -135,3 +140,9 @@ def idf_command(term: str) -> float:
     idx = InvertedIndex()
     idx.load()
     return idx.get_idf(term)
+
+
+def tfidf_command(doc_id: int, term: str) -> float:
+    idx = InvertedIndex()
+    idx.load()
+    return idx.get_tf_idf(doc_id, term)
