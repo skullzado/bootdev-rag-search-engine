77520d1fc33109aad98738568eb18fbf545766f1
skullzado <pafcalzado@gmail.com>
2026-01-21 13:39:26 +0800

    Add bm25_idf func, bm25idf command

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -8,6 +8,7 @@ from lib.keyword_search import (
     search_command,
     tf_command,
     tfidf_command,
+    bm25_idf_command,
 )
 
 
@@ -37,6 +38,13 @@ def main() -> None:
     tf_idf_parser.add_argument("doc_id", type=int, help="Document ID")
     tf_idf_parser.add_argument("term", type=str, help="Term to get TF-IDF score for")
 
+    bm25_idf_parser = subparsers.add_parser(
+        "bm25idf", help="Get BM25 IDF score for a given term"
+    )
+    bm25_idf_parser.add_argument(
+        "term", type=str, help="Term to get BM25 IDF score for"
+    )
+
     args = parser.parse_args()
 
     match args.command:
@@ -60,6 +68,9 @@ def main() -> None:
             print(
                 f"TF-IDF score of '{args.term}' in document '{args.doc_id}': {tf_idf:.2f}"
             )
+        case "bm25idf":
+            bm25idf = bm25_idf_command(args.term)
+            print(f"BM25 IDF score of '{args.term}': {bm25idf:.2f}")
         case _:
             parser.print_help()
 
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -79,6 +79,15 @@ class InvertedIndex:
         idf = self.get_idf(term)
         return tf * idf
 
+    def get_bm25_idf(self, term: str) -> float:
+        tokens = tokenize_text(term)
+        if len(tokens) != 1:
+            raise ValueError("term must be a single token")
+        token = tokens[0]
+        doc_count = len(self.docmap)
+        term_doc_count = len(self.index[token])
+        return math.log((doc_count - term_doc_count + 0.5) / (term_doc_count + 0.5) + 1)
+
 
 def build_command() -> None:
     idx = InvertedIndex()
@@ -146,3 +155,9 @@ def tfidf_command(doc_id: int, term: str) -> float:
     idx = InvertedIndex()
     idx.load()
     return idx.get_tf_idf(doc_id, term)
+
+
+def bm25_idf_command(term: str) -> float:
+    idx = InvertedIndex()
+    idx.load()
+    return idx.get_bm25_idf(term)
